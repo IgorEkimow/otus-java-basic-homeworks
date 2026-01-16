b5f8af8d87ca6e006980d81aba6330a646017cc2
IgorEkimov <igor.ekimov@paygine.net>
2026-01-16 15:14:49 +0300

    User class is done

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Main.java
@@ -0,0 +1,32 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.Period;
+
+public class Main {
+    public static void main(String[] args) {
+        User[] users = {
+            new User("Иванов", "Иван", "Иванович", "21.05.1958", "ivan@mail.ru"),
+            new User("Петров", "Петр", "Петрович", "12.03.1973", "petr@mail.ru"),
+            new User("Семенов", "Семен", "Семенович", "03.02.1980", "semen@mail.ru"),
+            new User("Сергеев", "Сергей", "Сергеевич", "09.09.1965", "sergey@mail.ru"),
+            new User("Алексеев", "Алексей", "Алексеевич", "18.05.2001", "alexey@mail.ru"),
+            new User("Юриев", "Юрий", "Юриевич", "24.06.1991", "uriy@mail.ru"),
+            new User("Максимов", "Максим", "Максимович", "20.01.2004", "max@mail.ru"),
+            new User("Степанов", "Степан", "Степанович", "10.07.1998", "stepan@mail.ru"),
+            new User("Борисов", "Борис", "Борисович", "19.04.1968", "boris@mail.ru"),
+            new User("Константинов", "Константин", "Константинович", "13.11.1987", "konstantin@mail.ru")
+        };
+
+        for (User user : users) {
+            LocalDate birthDate = LocalDate.parse(user.getDateOfBirth(), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            LocalDate todayDate = LocalDate.now();
+            Period period = Period.between(birthDate, todayDate);
+
+            if(period.getYears() > 40) {
+                user.info();
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/User.java
@@ -0,0 +1,64 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+public class User {
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String dateOfBirth;
+    private String email;
+
+    public User(String lastName, String firstName, String patronymic, String dateOfBirth, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void info() {
+        System.out.println("ФИО: " + lastName + " " + firstName + " " + patronymic);
+        System.out.println("Год рождения: " + dateOfBirth);
+        System.out.println("E-mail: " + email);
+        System.out.println();
+    }
+}
