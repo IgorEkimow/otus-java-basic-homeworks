1e3fcecb8c925aa5cb9195fec7104197b2e0026f
Igor Ekimow <igorekimow@gmail.com>
2026-01-25 15:18:44 +0300

    Initial homework 7

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework7/Human.java
@@ -0,0 +1,11 @@
+package ru.otus.java.basic.homeworks.homework7;
+
+public class Human {
+    public String name;
+    public String currentTransport;
+
+    public Human(String name, String currentTransport) {
+        this.name = name;
+        this.currentTransport = currentTransport;
+    }
+}
