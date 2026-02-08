d8c71ec1d5fbb87d30c7ddc69abb347bffa6bd2e
Igor Ekimow <igorekimow@gmail.com>
2026-02-08 19:49:45 +0300

    Homework №10 is done

--- src/main/java/ru/otus/java/basic/homeworks/homework10/PhoneBook.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework10/PhoneBook.java
@@ -1,5 +1,45 @@
 package ru.otus.java.basic.homeworks.homework10;
 
-public class PhoneBook {
+import java.util.Map;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.ArrayList;
 
+public class PhoneBook {
+    private final Map<String, List<String>> phoneBook;
+
+    public PhoneBook() {
+        this.phoneBook = new TreeMap<>();
+    }
+
+    public void add(String name, String phone) {
+        if (phoneBook.containsKey(name)) {
+            List<String> phones = phoneBook.get(name);
+            phones.add(phone);
+        } else {
+            List<String> phones = new ArrayList<>();
+            phones.add(phone);
+            phoneBook.put(name, phones);
+        }
+    }
+
+    public List<String> find(String name) {
+        return phoneBook.getOrDefault(name, new ArrayList<>());
+    }
+
+    public boolean containsPhoneNumber(String phone) {
+        for (List<String> phones : phoneBook.values()) {
+            if (phones.contains(phone)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void getPhoneBook() {
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
