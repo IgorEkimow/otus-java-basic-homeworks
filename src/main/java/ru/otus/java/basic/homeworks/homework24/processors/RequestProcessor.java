7097c9b73dfcf10948d432a5e9ab7e86b499ab14
Igor Ekimow <igorekimow@gmail.com>
2026-04-20 18:46:57 +0300

    Initial homework

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework24/processors/RequestProcessor.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework24.processors;
+
+import ru.otus.java.basic.homeworks.homework24.HttpRequest;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public interface RequestProcessor {
+    void execute(HttpRequest request, OutputStream output) throws IOException;
+}
